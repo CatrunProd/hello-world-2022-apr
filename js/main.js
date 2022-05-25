4418d4d2759974a38cde379ab709ce3f91de184c
Paul Sokolovskiy <paulsokolovskiy@gmail.com>
2022-05-25 16:06:37 +0000

    added footer line

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--active')
+    });
+
+    $('[data-fancybox]').fancybox({
+        youtube: {
+            controls: 1,
+            showinfo: 1
+        }
+    });
+});
+
+// Init blog slider
+$(function () {
+    $("#blog-slider").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        infinite: true,
+        prevArrow: '<div class="slick-prev"><i class="fa-solid fa-chevron-left"></i></div>',
+        nextArrow: '<div class="slick-next"><i class="fa-solid fa-chevron-right"></i></div>'
+    })
+})
