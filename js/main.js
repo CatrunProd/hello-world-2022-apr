de630a66606f0d9a93368f0046984f6494d287f4
Paul Sokolovskiy <paulsokolovskiy@gmail.com>
2022-05-27 17:20:06 +0000

    fix blog container for blog story

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-$(function () {
-
-    $('.menu__btn').on('click', function () {
-        $('.menu__list').toggleClass('menu__list--active')
-    });
-
-    $('[data-fancybox]').fancybox({
-        youtube: {
-            controls: 1,
-            showinfo: 1
-        }
-    });
-});
-
 // Init blog slider
 $(function () {
     $("#blog-slider").slick({
