93c4631f48e0feb5c2fa9b7be5924b3fc8526292
CatrunProd <paulsokolovskiy@gmail.com>
2022-07-15 18:35:39 +0300

    added main-products-carousel in main

--- js/product-list.js
+++ js/product-list.js
@@ -36,20 +36,28 @@
          const productsContainer = document.querySelector('.main-products__list');
          for (const product of products) {
             productsContainer.innerHTML += `
-            <article class="product-card">
-            <img class="product-card__image"
+            <div class="product-card">
+             <img class="product-card__image"
               src="${product.imgUrl}"
               alt="${product.name}"
-            />
-            <h3 class="product-card__h3">${product.name}</h3>
-            <p class="product-card__description">
-            ${product.description}
-            </p>
-            <div class="product-card__buttons">
-            <button class="product-card__buttons-info"> Info </button>
-            <button class="product-card__buttons-buy"> Buy - ${product.price} </button>
-            </div>
-            `;
+             />
+              <div class="card-body d-flex flex-column">
+              <h3 class="product-card__h3">${product.name}</h3>
+              <p class="product-card__description">
+              ${product.description}
+              </p>
+               <div class="d-flex justify-content-around">
+                <div class="product-card__buttons">
+                  <button class="product-card__buttons-info" data-bs-toggle="modal" 
+                  data-bs-target="#productInfoModal" data-id="${product.id}"> Info 
+                  </button>
+                  <button class="product-card__buttons-buy" data-id="${product.id}">  
+                  $${product.price} - Buy
+                  </button>
+               </div>
+              </div>
+             </div>
+            </div>`;
          }
     }
 
---
+++ js/products-carousel.js
@@ -0,0 +1,25 @@
+(function(){
+    const slides = [
+       '<div class="slide"><img src="img/products-img/baby-yoda.svg" alt="Baby-Yoda"></div>',
+       '<div class="slide"><img src="img/products-img/banana.svg" alt="Banana"></div>',
+       '<div class="slide"><img src="img/products-img/girl.svg" alt="Girl"></div>',
+       '<div class="slide"><img src="img/products-img/viking.svg" alt="Viking"></div>',
+    ];
+
+    let currentSlide = 0;
+
+    function renderCarousel() {
+          const carouselContainer = document.querySelector('.main-products-carousel');
+          carouselContainer.innerHTML = slides[currentSlide];
+    }
+
+    function nextSlide() {
+        currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+        renderCarousel();
+    }
+
+    setInterval(nextSlide, 3000);
+
+    renderCarousel();
+ 
+})();
